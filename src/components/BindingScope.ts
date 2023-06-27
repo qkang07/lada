a4af8783f11340f04b3ef8334af7f1be599dc0e3
qikang.yuan <qikang.yuan@huolala.cn>
2023-06-27 18:51:19 +0800

    s

--- src/components/BindingScope.ts
+++ src/components/BindingScope.ts
@@ -1,16 +1,35 @@
-import { CompDefBase, CompInstanceBase, DefManager } from "./compDef";
+import { CompDefBase, CompInstanceBase, CompSchemaBase, DefManager } from "./compDef";
 
+/**
+ * Binding Scope ， 即「绑定」的容器，容器内的组件在此处理绑定信息。
+ */
 export class BindingScope {
   compMap: Map<string, CompInstanceBase> = new Map()
+
+  // action 的map，用于当 event 触发时，找到对应的 action
+  actionMap: Map<string, CompInstanceBase> = new Map()
+
+  // prop 的 map，用于当 state 改变时，找到对应的 prop
+  propMap: Map<string, CompInstanceBase> = new Map()
 
   constructor(public man: DefManager<CompDefBase>) {
 
   }
 
-  regComp() {
+  regComp(comp: CompInstanceBase) {
+    this.compMap.set(comp.id, comp)
+  }
+
+  triggerAction(id: string ,action: string) {
 
   }
-  makeFromSchema() {
+
+  updateProp(id: string, prop: string, value: any) {
+    const comp = this.compMap.get(id)
+
+  }
+
+  makeFromSchema(schema: CompSchemaBase) {
     
   }
 }
