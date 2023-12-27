78f35a3142df4a6cd18790926b63ce803749c120
qikang.yuan <qikang.yuan@huolala.cn>
2023-12-27 22:33:23 +0800

    s

--- src/components/Binding/BindingMember/index.tsx
+++ src/components/Binding/BindingMember/index.tsx
@@ -4,12 +4,18 @@ import { CompSchemaBase } from '@/libs/core/Def'
 
 type Props = {
   schema: CompSchemaBase
+  type: 'source' | 'target'
+  prop: string
+  onClick?:() => void
 }
 
 const BindingMember = (props: Props) => {
   const {schema} = props
   return (
-    <div className={styles.bindingMember}>
+    <div className={styles.bindingMember} onClick={() => {
+      console.log(schema)
+      props.onClick?.()
+    }}>
       <div className={styles.name}>{schema.name}</div>
       <div className={styles.id}>{schema.id}</div>
       <div className={styles.provider}>{schema.provider}</div>
--- src/components/Binding/index.tsx
+++ src/components/Binding/index.tsx
@@ -1,9 +1,10 @@
 import { BindingSchema, BindingScopeSchema, DataFilterSchema } from '@/libs/core/Def'
-import React, { useContext, useState } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
 import { DesignerContext } from '../Designer'
 import { CanvasContext } from '../Canvas/context'
 import styles from './index.module.less'
 import FilterCode from './Filter/FilterCode'
+import BindingMember from './BindingMember'
 
 type Props = {
   schema: BindingSchema
@@ -14,14 +15,22 @@ const Binding = (props: Props) => {
   const {schema} = props
   const [detailType, setDetailType] = useState<'source'|'filter'|'target'|undefined>()
   const [filter, setFilter] = useState<DataFilterSchema>()
+  const {sourceComp, targetComp} = useMemo(() => {
+    const sourceComp = bdCon?.schemaAgentMap.get(schema.source.id)![0].schema
+    const targetComp = bdCon?.schemaAgentMap.get(schema.source.id)![0].schema
+    return {sourceComp, targetComp}
+  }, [schema])
   return (
     <div>
       <div className={styles.binding}>
         <div className={styles.source}>
-          <div></div>
+          <BindingMember schema={sourceComp!} type='source' prop={schema.source.prop} />
         </div>
         <div className={styles.filters}></div>
-        <div className={styles.target}></div>
+        <div className={styles.target}>
+        <BindingMember schema={targetComp!} type='target' prop={schema.target.prop} />
+
+        </div>
       </div>
       <div className={styles.details}>
         {(detailType === 'source' || detailType === 'target') && <div>
